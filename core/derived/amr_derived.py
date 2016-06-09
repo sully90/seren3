0bce46f5c48150068cffef0c5af70928ad1ade6f
David Sullivan <dsullivan0190@gmail.com>
2016-06-09 17:25:28 +0100

    Description for cs field

--- core/derived/amr_derived.py
+++ core/derived/amr_derived.py
@@ -27,6 +27,9 @@ def amr_xHI(context, dset):
 
 @seren3.derived_quantity(requires=["P", "rho"])
 def amr_cs(context, dset):
+    '''
+    Gas sound speed in m/s (units are convertabke)
+    '''
     val = np.sqrt(1.66667 * dset['P'] / dset['rho'])
     unit = context.info['unit_velocity']
     result = SimArray(val * unit.express(context.C.m / context.C.s)\
