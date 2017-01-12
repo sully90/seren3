d9d54b498a5b09b6a44ac53932cf9a93304d16c3
David Sullivan <ds381@boole.uscs.susx.ac.uk>
2017-01-12 10:46:21 +0000

    Bug fixes

--- analysis/raytracing/optical_depth_tracer.py
+++ analysis/raytracing/optical_depth_tracer.py
@@ -140,7 +140,7 @@ class OpticalDepthTracer(DataProcessor):
     def __init__(self, seren_snapshot, verbose=None):
         source = seren_snapshot.g["nHI"].pymses_source
         ramses_output_info = seren_snapshot.ro.info
-        op = ScalarOperator(lambda dset: dset["rho"] * (1. - dset["xHII"] * sigma_alpha), ramses_output_info["unit_density"])
+        op = ScalarOperator(lambda dset: dset["rho"] * (1. - dset["xHII"] * sigma_alpha), ramses_output_info["unit_density"].express(seren_snapshot.C.H_cc))
         super(OpticalDepthTracer, self).__init__(source, op, amr_mandatory=True, verbose=verbose)
         self._ro_info = ramses_output_info
         self._cells_source = None
