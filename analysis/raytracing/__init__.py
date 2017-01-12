b7403435e142ea358f3cf8af511ba9fca29aef4d
David Sullivan <ds381@boole.uscs.susx.ac.uk>
2017-01-12 09:31:32 +0000

    raytracing init

---
+++ analysis/raytracing/__init__.py
@@ -0,0 +1,6 @@
+r"""
+:mod:`seren3.analysis.raytracing` --- 2D map ray-casting module
+===============================================================
+
+"""
+from optical_depth_tracer import OpticalDepthTracer
