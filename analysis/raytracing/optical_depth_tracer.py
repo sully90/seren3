c0044dd4562a4df5f45690682eb311f569c0ac19
David Sullivan <ds381@boole.uscs.susx.ac.uk>
2017-01-16 10:58:10 +0000

    Optical depth fixes

--- analysis/raytracing/optical_depth_tracer.py
+++ analysis/raytracing/optical_depth_tracer.py
@@ -144,7 +144,7 @@ class OpticalDepthTracer(DataProcessor):
         ramses_output_info = seren_snapshot.ro.info
 
         nH_fn = derived_utils.get_derived_field("amr", "nH")
-        nHI_fn = lambda dset: nH_fn(seren_snapshot, dset).in_units("m**-3") * (1. - dset["xHII"])
+        nHI_fn = lambda dset: nHI_fn(seren_snapshot, dset).in_units("m**-3") * (1. - dset["xHII"])
 
         op = ScalarOperator(lambda dset: sigma_alpha * nH_fn(dset), seren_snapshot.C.m**-3)
         super(OpticalDepthTracer, self).__init__(source, op, amr_mandatory=True, verbose=verbose)
